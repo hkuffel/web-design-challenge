cb0c0cc45c25893d0cddde950501d7aa36ecca78
Hunter Kuffel <hkuffel@alumni.nd.edu>
2019-10-06 18:12:38 -0400

    removed unneeded column from data page

--- table.py
+++ table.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv("../HW/api_hw/output_data/cities.csv")
-df.to_html("table.html")
+df = pd.read_csv("Resources/cities.csv")
+df = df.drop("Unnamed: 0", axis=1)
+df.to_html("table.html", index=False)
